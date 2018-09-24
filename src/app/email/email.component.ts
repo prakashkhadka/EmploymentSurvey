e3819cd4a7e691fa919e36b1da50e98cf500ee04
Prakash Khadka <prakash@Prakashs-MacBook-Pro.local>
2018-09-24 23:34:49 +1000

    Thank you messege added with a good consultion

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -22,14 +22,48 @@ export class EmailComponent implements OnInit {
  
 
   onSubmit(){
+  	/*
+  	let pattern = new RegExp("^(?=.*[A-Z])(?=.*[0-9])");
+  	
+  	if(pattern.test(this.surveyService.email)){
+      this.isInvalid = false;
+      //console.log("Password inalid");
+    }
+  	 
     if(this.surveyService.consent){
-      this.surveyService.submit();
-      
+
+      	this.surveyService.submit();
+
+
     }
     else{
       this.isInvalid = true;
     }
+    */
+
+    if(this.surveyService.email && this.surveyService.consent){
+    	//console.log(this.surveyService.email);
+    	//let pattern = new RegExp("^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$");
+    	let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
+    	if(pattern.test(this.surveyService.email)){
+    		console.log("email passed");
+	    	this.surveyService.submit();
+	    }
+	    else{
+	    	console.log("Couldn't pass regex");
+	    	this.isInvalid = true;
+	    }
+    }
+    else if(this.surveyService.consent){
+    	console.log("can go")
+		this.surveyService.submit();
+    }
+    else{
+    	this.isInvalid = true;
+    }
   }
+
+
 
   goBack(){
   	this.location.back();
